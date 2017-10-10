edb4b5c9e644c316f1e5fcc2b497da5779dddc91
Denny Harijanto <denny@nusantara-cloud.com>
2017-10-10 16:08:39 +0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+var dt = require('datatables.net')
+var $ = require('jquery')
+var unique = require('uniq')
+
+class MyClass {
+  constructor (data) {
+    this.data = data
+  }
+
+  getData () {
+    return this.data
+  }
+
+  sumData () {
+    return this.data.reduce((acc, val) => {
+      return acc + val
+    }, 0)
+  }
+}
+
+const c = new MyClass([1, 2, 2, 3, 4, 5, 5, 5, 6])
+console.log(c.getData())
+
+setTimeout(() => {
+  $('#heading').html(`Sum of ${c.getData().join(', ')} is ${c.sumData()}`)
+}, 1000)
